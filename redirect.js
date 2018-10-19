bfe73d698538bbdf82eef4472dfa3f63e3b8ed42
Ben Willis <B01w234@gmail.com>
2018-10-20 00:32:06 +0100

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-(() = {
+
 let websites = ['https://padlet.com/kja1/3owbsv8a7hhf',
 'https://www.youtube.com/playlist?list=PLxqj8BfO0YvxwQVrtfRVaaDTxfQFXhd1S',
 'https://www.youtube.com/playlist?list=PL04uZ7242_M6yYkM_hA2mzBZGcvEnLSOU',
@@ -6,6 +6,6 @@ let websites = ['https://padlet.com/kja1/3owbsv8a7hhf',
 'https://www.youtube.com/playlist?list=PL04uZ7242_M6yJQRfFcPUhAxykFNuPQy6'];
 
 let rand Math.floor(Math.random() * websites.length); 
-
+alert('gey');
 window.location = websites[rand];
-});
+
