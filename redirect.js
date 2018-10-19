e1d2bd32839573f19353933b27e1a1c582d2ba93
Ben Willis <B01w234@gmail.com>
2018-10-19 23:11:07 +0100

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,9 @@
+let websites = ['https://padlet.com/kja1/3owbsv8a7hhf',
+'https://www.youtube.com/playlist?list=PLxqj8BfO0YvxwQVrtfRVaaDTxfQFXhd1S',
+'https://www.youtube.com/playlist?list=PL04uZ7242_M6yYkM_hA2mzBZGcvEnLSOU',
+'https://www.youtube.com/playlist?list=PL04uZ7242_M6yYkM_hA2mzBZGcvEnLSOU',
+'https://www.youtube.com/playlist?list=PL04uZ7242_M6yJQRfFcPUhAxykFNuPQy6'];
+
+let rand Math.floor(Math.random() * websites.length); 
+
+window.location = websites[rand];
